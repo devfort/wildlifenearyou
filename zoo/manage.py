fc5b8f07f84cbeefc0373ececc03bf7075661884
Simon Willison <simon@simonwillison.net>
2008-11-22 19:22:11 +0000

    Fixed bug in manage.py

--- zoo/manage.py
+++ zoo/manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'ext')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'ext'))
 
 from django.core.management import execute_manager
 try:
