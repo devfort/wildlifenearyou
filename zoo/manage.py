f1156470cf9a2f2b109acffed0a5c51187959e1b
Simon Willison <simon@simonwillison.net>
2008-11-22 19:16:16 +0000

    Added /ext/ directory to put dependencies in

--- zoo/manage.py
+++ zoo/manage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import os, sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'ext')
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
