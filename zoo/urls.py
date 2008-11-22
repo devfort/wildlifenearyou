c8fb192fccdef4643cd76f0ad05a384e55ee9146
Simon Willison <simon@simonwillison.net>
2008-11-22 19:48:00 +0000

    Configured static file serving, added jQuery just in case

--- zoo/urls.py
+++ zoo/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+
+import os
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -12,6 +15,9 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': os.path.join(settings.OUR_ROOT, 'static')
+    }),
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
 )
