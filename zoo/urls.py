a6a0fa50e58f29d22f29b07a52cc7d5b7565c9a3
Simon Willison <simon@simonwillison.net>
2008-11-23 18:27:46 +0000

    Dodo = 410

--- zoo/urls.py
+++ zoo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django import http
 
 import os
 
@@ -9,15 +10,23 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^zoo/', include('zoo.foo.urls')),
-
+    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': os.path.join(settings.OUR_ROOT, 'static')
     }),
-
+    
+    # Dodo
+    (r'^animals/dodo/$', 
+        lambda r: http.HttpResponseGone('410 Gone')
+    ),
+    (r'^latin/raphus-cucullatus/$', 
+        lambda r: http.HttpResponseRedirect('/animals/dodo/')
+    ),
+    
     # Django built-in admin
     (r'^admin/(.*)', admin.site.root),
 )
