1d05c00616f52c7a25489e720918e1866628d135
Matthew Harris <matt@themattharris.com>
2008-11-24 12:26:35 +0000

    Added prepopulate for animal common_name slug, and reorigranised model class to match places

--- zoo/animals/admin.py
+++ zoo/animals/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from zoo.animals.models import Animal, AnimalClass
 
-class AnimalAdmin(admin.ModelAdmin):
-    list_display = ('common_name', 'animal_class', 'latin_name', 'slug')
-    list_filter = ['animal_class']
-    search_fields = ['common_name', 'latin_name']
-
 class AnimalClassAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Animal, AnimalAdmin)
+admin.site.register(Animal, 
+    list_display = ('common_name', 'animal_class', 'latin_name', 'slug'),
+    list_filter = ['animal_class'],
+    search_fields = ['common_name', 'latin_name'],
+    prepopulated_fields = {'slug': ('common_name',)},
+)
 admin.site.register(AnimalClass, AnimalClassAdmin)
