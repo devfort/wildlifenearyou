450ef5cbfe6316882a9f9f9c8b8eb4b793bfd2eb
Aaron Sokoloski <asokoloski@gmail.com>
2008-11-24 17:06:05 +0000

    oops, forgot migration to drop user id from profile

---
+++ zoo/migrations/031_drop_user_id_from_profile.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+if settings.DATABASE_ENGINE == 'mysql':
+    from dmigrations.mysql import migrations as m
+elif settings.DATABASE_ENGINE == 'sqlite3':
+    from dmigrations.sqlite3 import migrations as m
+
+class CustomMigration(m.Migration):
+    def __init__(self):
+        sql_up = ['''
+        ALTER TABLE accounts_profile DROP COLUMN user_level_id;
+        ''']
+        sql_down = ['''
+        ALTER TABLE accounts_profile ADD COLUMN user_level_id INTEGER NOT NULL;
+        ''']
+        super(CustomMigration, self).__init__(
+            sql_up=sql_up, sql_down=sql_down
+        )
+    # Or override the up() and down() methods
+
+migration = CustomMigration()
