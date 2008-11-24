a024a8da95fb491e4a3bf67bcde701b185698bba
Simon Willison <simon@simonwillison.net>
2008-11-24 17:29:59 +0000

    Photo admin niceties

--- zoo/photos/admin.py
+++ zoo/photos/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from zoo.photos.models import Photo
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'preview', 'created_by', 'created_at')
+    list_filter = ('created_at',)
+    
+    def preview(self, obj):
+        return obj.photo.thumbnail_tag
+    preview.allow_tags = True
+
+admin.site.register(Photo, PhotoAdmin)
