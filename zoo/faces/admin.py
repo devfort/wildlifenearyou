988cab0c965f5324a320aae4bf776454fe6ec062
Simon Willison <simon@simonwillison.net>
2008-11-24 17:24:44 +0000

    Show a preview of each face part image on http://10.0.1.5:8000/admin/faces/facepart/ in the admin

--- zoo/faces/admin.py
+++ zoo/faces/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from zoo.faces.models import FaceArea, FacePart
 
+class FacePartAdmin(admin.ModelAdmin):
+    list_display = ('image', 'area', 'preview')
+    
+    def preview(self, obj):
+        return u'<img src="%s" width="150">' % obj.image.url
+    preview.allow_tags = True
+    
 admin.site.register(FaceArea)
-admin.site.register(FacePart)
+admin.site.register(FacePart, FacePartAdmin)
 
