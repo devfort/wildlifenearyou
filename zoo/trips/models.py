60bfb51f0939c686f8479f7c4a26c438bbdc01e8
Ben Phillips <djbenji@gmail.com>
2008-11-24 21:43:00 +0000

    Trip.get_passport() now works for anonymous users

--- zoo/trips/models.py
+++ zoo/trips/models.py
@@ -13,19 +13,22 @@ class Trip(models.Model):
     user = models.ForeignKey(User, related_name='trips')
     name = models.CharField(null=True, blank=True, max_length=100)
     sightings = models.ManyToManyField(Species, through='TripSighting')
-    
+
     def save(self, *args, **kwargs):
         if self.end==None:
             self.end = self.start + datetime.timedelta(1)
         super(Trip,self).save(args, kwargs)
-    
+
     #THIS MUST BE static.  It does not act on an instance and is called from the profile model
     @staticmethod
     def get_passport(user):
         class Passport:
             def __init__(self, seen):
                 self.seen_species = seen
-        
+
+        if not user.is_authenticated():
+            return Passport([])
+
         species_list = list(Species.objects.filter(trip__user=user))
         by_count = {}
         for species in species_list:
@@ -40,20 +43,20 @@ class Trip(models.Model):
 
         o = Passport(species_list)
         return o
-        
+
     def title(self):
         if self.name:
             return self
         else:
             #FIX ME - Need I18N
             return u'your trip to %s' % (self.place.known_as)
-        
+
     def __unicode__(self):
         return self.user.username + u'@' + self.place.known_as
 
 class TripSighting(models.Model):
     trip = models.ForeignKey(Trip)
     species = models.ForeignKey(Species)
-    
+
     def __unicode__(self):
         return u'' + unicode(self.trip) + u': ' + self.species.common_name
