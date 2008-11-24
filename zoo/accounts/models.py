9ec9da60a14adbe0331cd4bf14a2de232ae9239f
Matthew Harris <matt@themattharris.com>
2008-11-24 19:20:52 +0000

    Added animal passport seen species to profile page

--- zoo/accounts/models.py
+++ zoo/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from zoo.utils import attrproperty
+from zoo.animals.models import Species
+from zoo.trips.models import TripSighting
 
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
@@ -19,3 +21,24 @@ class Profile(models.Model):
 
     def __unicode__(self):
         return u'Profile for %s' % (self.user)
+
+    # the user's animal passport
+    def passport(self):
+        class Passport:
+            def __init__(self, seen):
+                self.seen_species = seen
+
+        species_list = list(Species.objects.filter(trip__user=self.user))
+        by_count = {}
+        for species in species_list:
+            by_count[species] = by_count.get(species, 0) + 1
+            
+        species_list = by_count.keys()
+        
+        for species in species_list:
+            species.count = by_count[species]
+
+        species_list.sort(key=lambda s:s.count, reverse=True)
+        
+        o = Passport(species_list)
+        return o
