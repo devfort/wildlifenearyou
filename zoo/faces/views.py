f43ae15b67341382467b5750c423460641e331b3
Aaron Sokoloski <asokoloski@gmail.com>
2008-11-25 23:23:35 +0000

    alternate import for ElementTree

--- zoo/faces/views.py
+++ zoo/faces/views.py
@@ -38,3 +38,36 @@ def profile_images_xml(request):
         ET.tostring(profileImages), 
         content_type = 'application/xml; charset=utf8'
     )
+
+"""
+<?xml version="1.0" encoding="utf-8"?>
+<profileImages>
+	<faces>
+		<face src="face1.png" id="uid" title="pale skin" />	
+	</faces>
+	<eyes>
+		<eye src="eye1.png" id="uid" title="blue eyes" />
+	</eyes>
+	<noses>
+		<nose src="nose1.png" id="uid" title="nose 1" />
+	</noses>
+	<ears>
+		<ear src="ear1.png" id="uid" title="ear 1" />
+	</ears>
+	<mouths>
+		<mouth src="mouth1.png" id="uid" title="mouth 1" />
+	</mouths>
+	<cheeks>
+		<cheek src="cheeck1.png" id="uid" title="cheek 1" />
+	</cheeks>
+	<facialHairs>
+		<facialHair src="facialHair1.png" id="uid" title="facial Hair  1" />
+	</facialHairs>
+	<hairs>
+		<hair src="hair1.png" id="uid" title="Hair  1" />
+	</hairs>
+	<hairAccessories>
+		<hairAccessorie src="hairaccessory1.png" id="uid" title="Hair Accessorie 1" />
+	</hairAccessories>
+</profileImages>
+"""
