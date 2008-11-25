7ca79713aac96d9ef1e8dfb360784f2308ecd6e0
James Aylett <james@tartarus.org>
2008-11-25 03:37:13 +0000

    Correct migration to use `` instead of "" to surround SQL identifiers.

--- zoo/migrations/044_zoo_places_models.py
+++ zoo/migrations/044_zoo_places_models.py
@@ -5,16 +5,16 @@ elif settings.DATABASE_ENGINE == 'sqlite3':
     from dmigrations.sqlite3 import migrations as m
 import datetime
 migration = m.Migration(sql_up=["""
-    CREATE TABLE "places_placeopening" (
-        "id" integer NOT NULL PRIMARY KEY,
-        "start_date" date NULL,
-        "end_date" date NULL,
-        "days_of_week" varchar(100) NOT NULL,
-        "times" varchar(100) NOT NULL,
-        "closed" bool NOT NULL,
-        "place_id" integer NOT NULL
+    CREATE TABLE `places_placeopening` (
+        `id` integer NOT NULL PRIMARY KEY,
+        `start_date` date NULL,
+        `end_date` date NULL,
+        `days_of_week` varchar(100) NOT NULL,
+        `times` varchar(100) NOT NULL,
+        `closed` bool NOT NULL,
+        `place_id` integer NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8
     ;
 """], sql_down=["""
-    DROP TABLE "places_placeopening";
+    DROP TABLE `places_placeopening`;
 """])
