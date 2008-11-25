b2dc9a548012a3399c44ebcbbd1011ef6b2bac96
Matthew Somerville <matthew@dracos.co.uk>
2008-11-25 14:05:52 +0000

    Shared exclude list

--- zoo/models.py
+++ zoo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+exclude = ['created_at', 'created_by','modified_at', 'modified_by']
+
 class AuditedModel(models.Model):
     created_at = models.DateTimeField(null=False, blank=False)
     created_by = models.ForeignKey(User, related_name='created_%(class)s_set', null=False, blank=False)
--- zoo/trips/admin.py
+++ zoo/trips/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from zoo.trips.models import Trip, Sighting
+from zoo.models import exclude
 
 class SightingInline(admin.TabularInline):
     model = Sighting
-
+    exclude = exclude
 
 admin.site.register(Trip,
-                   inlines = [SightingInline],
-                    )
-admin.site.register(Sighting)
+    inlines = [SightingInline],
+    exclude = exclude,
+)
+admin.site.register(Sighting,
+    exclude = exclude,
+)
+
