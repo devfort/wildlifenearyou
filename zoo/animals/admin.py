9319247ce99413dd7dd7ca2707304c6c9a48fe52
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-25 13:19:22 +0000

    Rename s/excludees/excludes/g

--- zoo/animals/admin.py
+++ zoo/animals/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from zoo.animals.models import Species, SpeciesGroup, SuperSpecies
 
-excludees = ['created_at', 'created_by', 'modified_at', 'modified_by']
+excludes = ['created_at', 'created_by', 'modified_at', 'modified_by']
 
 admin.site.register(Species,
     list_display = ('common_name', 'species_group', 'latin_name', 'slug'),
     list_filter = ['species_group'],
     search_fields = ['common_name', 'latin_name'],
     prepopulated_fields = {'slug': ('common_name',)},
-    exclude = excludees,
+    exclude = excludes,
 )
 admin.site.register(SpeciesGroup)
 admin.site.register(SuperSpecies,
-    exclude = excludees,
+    exclude = excludes,
     prepopulated_fields = {'slug': ('common_name',)},
 )
