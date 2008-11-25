242669abbc7d101d141eda488923740bdd41f4ec
Matthew Somerville <matthew@dracos.co.uk>
2008-11-25 11:51:32 +0000

    Correct quotes

--- zoo/migrations/048_django_contrib_flatpages_models.py
+++ zoo/migrations/048_django_contrib_flatpages_models.py
@@ -5,26 +5,26 @@ elif settings.DATABASE_ENGINE == 'sqlite3':
     from dmigrations.sqlite3 import migrations as m
 import datetime
 migration = m.Migration(sql_up=["""
-    CREATE TABLE "django_flatpage" (
-        "id" integer NOT NULL PRIMARY KEY,
-        "url" varchar(100) NOT NULL,
-        "title" varchar(200) NOT NULL,
-        "content" text NOT NULL,
-        "enable_comments" bool NOT NULL,
-        "template_name" varchar(70) NOT NULL,
-        "registration_required" bool NOT NULL
+    CREATE TABLE `django_flatpage` (
+        `id` integer NOT NULL PRIMARY KEY,
+        `url` varchar(100) NOT NULL,
+        `title` varchar(200) NOT NULL,
+        `content` text NOT NULL,
+        `enable_comments` bool NOT NULL,
+        `template_name` varchar(70) NOT NULL,
+        `registration_required` bool NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8
     ;
 """, """
-    CREATE TABLE "django_flatpage_sites" (
-        "id" integer NOT NULL PRIMARY KEY,
-        "flatpage_id" integer NOT NULL REFERENCES "django_flatpage" ("id"),
-        "site_id" integer NOT NULL REFERENCES "django_site" ("id"),
-        UNIQUE ("flatpage_id", "site_id")
+    CREATE TABLE `django_flatpage_sites` (
+        `id` integer NOT NULL PRIMARY KEY,
+        `flatpage_id` integer NOT NULL REFERENCES `django_flatpage` (`id`),
+        `site_id` integer NOT NULL REFERENCES `django_site` (`id`),
+        UNIQUE (`flatpage_id`, `site_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8
     ;
 """], sql_down=["""
-    DROP TABLE "django_flatpage_sites";
+    DROP TABLE `django_flatpage_sites`;
 """, """
-    DROP TABLE "django_flatpage";
+    DROP TABLE `django_flatpage`;
 """])
