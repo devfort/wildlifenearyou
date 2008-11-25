f49cdca336e9c0c62c9b4a413c8641fcbaf64a2d
Matthew Somerville <matthew@dracos.co.uk>
2008-11-25 13:59:31 +0000

    Edit sightings from trip admin page

--- zoo/trips/admin.py
+++ zoo/trips/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from zoo.trips.models import Trip, Sighting
 
-#class TripSightingInline(admin.TabularInline):
-#    model = TripSighting
+class SightingInline(admin.TabularInline):
+    model = Sighting
 
 
 admin.site.register(Trip,
-#                   inlines = [TripSightingInline],
+                   inlines = [SightingInline],
                     )
 admin.site.register(Sighting)
