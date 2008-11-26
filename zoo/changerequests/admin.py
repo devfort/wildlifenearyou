095e22b49ec6c54f2c889c7e6319380ab12d85f7
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-26 23:24:37 +0000

    Add admin for CreateObjectRequest.

--- zoo/changerequests/admin.py
+++ zoo/changerequests/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
 from zoo.changerequests.models import ChangeRequestGroup, ChangeRequest, \
-    ChangeAttributeRequest, DeleteObjectRequest
+    ChangeAttributeRequest, DeleteObjectRequest, CreateObjectRequest
 
 class ChangeRequestAdmin(admin.ModelAdmin): pass
 class ChangeRequestGroupAdmin(admin.ModelAdmin): pass
 class ChangeAttributeRequestAdmin(admin.ModelAdmin): pass
 class DeleteObjectRequestAdmin(admin.ModelAdmin): pass
+class CreateObjectRequestAdmin(admin.ModelAdmin): pass
 
 admin.site.register(ChangeRequest, ChangeRequestAdmin)
 admin.site.register(ChangeRequestGroup, ChangeRequestGroupAdmin)
 admin.site.register(ChangeAttributeRequest, ChangeAttributeRequestAdmin)
 admin.site.register(DeleteObjectRequest, DeleteObjectRequestAdmin)
+admin.site.register(CreateObjectRequest, CreateObjectRequestAdmin)
