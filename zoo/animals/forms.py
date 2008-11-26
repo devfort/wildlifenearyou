f0a0c1059289eb56f7bcd6c9692e6466c0faa498
Simon Willison <simon@simonwillison.net>
2008-11-26 14:06:46 +0000

    Added missing file

---
+++ zoo/animals/forms.py
@@ -0,0 +1,16 @@
+from zoo.search_picker import SearchPickerField
+from models import Species
+
+class SpeciesField(SearchPickerField):
+    pick_one_text = '-- pick a species --'
+    def search(self, q):
+        return [
+            (s.id, s.common_name)
+            for s in Species.objects.filter(common_name__icontains = q)
+        ]
+    
+    def is_valid_choice(self, value):
+        return Species.objects.filter(id = value).count()
+
+
+    
