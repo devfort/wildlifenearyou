9d8dda3e069610b0b0a59ef5c63ba1572a2e4d2f
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-26 18:13:43 +0000

    Ensure only staff members can moderate stuff.

--- zoo/changerequests/views.py
+++ zoo/changerequests/views.py
@@ -1,13 +1,18 @@
-from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
 from zoo.shortcuts import render
 
 from zoo.changerequests.forms import ChangeRequestActionForm
 from zoo.changerequests.models import ChangeRequestGroup, ChangeRequest
 
+@login_required
 def moderation_queue(request):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
     if request.method == 'POST':
         form = ChangeRequestActionForm(request.POST)
 
