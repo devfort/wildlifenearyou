a44cae87319d9f30f7547598d4d53590d612a478
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-26 18:39:09 +0000

    Add DeleteObjectRequest to admin.

--- zoo/changerequests/admin.py
+++ zoo/changerequests/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from zoo.changerequests.models import ChangeRequestGroup, ChangeRequest, \
-    ChangeAttributeRequest
+    ChangeAttributeRequest, DeleteObjectRequest
 
 class ChangeRequestAdmin(admin.ModelAdmin): pass
 class ChangeRequestGroupAdmin(admin.ModelAdmin): pass
 class ChangeAttributeRequestAdmin(admin.ModelAdmin): pass
+class DeleteObjectRequestAdmin(admin.ModelAdmin): pass
 
 admin.site.register(ChangeRequest, ChangeRequestAdmin)
 admin.site.register(ChangeRequestGroup, ChangeRequestGroupAdmin)
 admin.site.register(ChangeAttributeRequest, ChangeAttributeRequestAdmin)
+admin.site.register(DeleteObjectRequest, DeleteObjectRequestAdmin)
