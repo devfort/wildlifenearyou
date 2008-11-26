2875d21ac4ba160b641640eda69c71d56d4998c6
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-26 18:13:41 +0000

    Detect race conditions when (soft-) applying a changerequest when it conflicts.

--- zoo/changerequests/forms.py
+++ zoo/changerequests/forms.py
@@ -5,3 +5,12 @@ from zoo.changerequests.models import ChangeRequest
 class ChangeRequestActionForm(forms.Form):
     action = forms.ChoiceField(choices=[(x, x) for x in ('apply', 'delete', 'force')])
     changerequest = forms.ModelChoiceField(ChangeRequest.objects.all())
+
+    def clean(self):
+        action = self.cleaned_data.get('action', None)
+        cr = self.cleaned_data.get('changerequest', None)
+
+        if action == 'apply' and cr and cr.get_real().is_conflict():
+            raise forms.ValidationError("Race condition.")
+
+        return self.cleaned_data
