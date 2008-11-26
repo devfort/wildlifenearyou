403e371e8b3894f7f3326eb0f2e6dc31b77a5cd4
Ben Phillips <djbenji@gmail.com>
2008-11-26 11:57:37 +0000

    Add random zoo to home page

--- zoo/homepage/views.py
+++ zoo/homepage/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseServerError
 from zoo.shortcuts import render
 
+from zoo.places.models import Place
+
 def landing(request):
+    random_zoo = Place.objects.all().order_by('?')[0]
     return render(request, 'homepage/landing.html', {
+            'random_zoo': random_zoo,
     })
