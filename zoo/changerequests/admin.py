b22fa35bc8742ddfad29f1d20ecc30f89f2e6b0f
Chris Lamb <chris@chris-lamb.co.uk>
2008-11-26 13:43:29 +0000

    Add admin for ChangeAttributeRequest.

--- zoo/changerequests/admin.py
+++ zoo/changerequests/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from zoo.changerequests.models import ChangeRequestGroup, ChangeRequest
+from zoo.changerequests.models import ChangeRequestGroup, ChangeRequest, \
+    ChangeAttributeRequest
 
 class ChangeRequestAdmin(admin.ModelAdmin): pass
 class ChangeRequestGroupAdmin(admin.ModelAdmin): pass
+class ChangeAttributeRequestAdmin(admin.ModelAdmin): pass
 
 admin.site.register(ChangeRequest, ChangeRequestAdmin)
 admin.site.register(ChangeRequestGroup, ChangeRequestGroupAdmin)
+admin.site.register(ChangeAttributeRequest, ChangeAttributeRequestAdmin)
