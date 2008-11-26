7a799d56bc794158870878a3c8e4ddf51d75fd3d
Richard Boulton <richard@tartarus.org>
2008-11-26 14:38:09 +0000

    Update client with new params.

--- zoo/ext/djxappy_client/__init__.py
+++ zoo/ext/djxappy_client/__init__.py
@@ -2,5 +2,5 @@
 from document import Document
 from errors import SearchClientError
 from field import Field
-from query import Query, QueryPart, TextQuery
+from query import Query, QueryPart, FreeTextQuery, GeoDistanceQuery
 from client import XappyClient
--- zoo/ext/djxappy_client/query.py
+++ zoo/ext/djxappy_client/query.py
@@ -12,14 +12,14 @@ class Query(object):
         """Create the query object.
 
         If `text` is supplied, it is treated as a text string, and is
-        equivalent to setting the query definition to TextQuery(text).
+        equivalent to setting the query definition to FreeTextQuery(text).
 
         """
         self.opts = {}
         self.part = None
 
         if text is not None:
-            self.part = TextQuery(text)
+            self.part = FreeTextQuery(text)
 
     def sort_by(self, field_name, ascending=True):
         """Set the field to sort by.
@@ -41,20 +41,33 @@ class QueryPart(object):
     def to_params(self):
         raise NotImplementedError('Implement this method in subclass.')
 
-class TextQuery(QueryPart):
-    """A query object.
+class FreeTextQuery(QueryPart):
+    """A query which searches for some free text.
 
     """
-    def __init__(self, query_string=None):
+    def __init__(self, text=None):
         """Create a query object.
 
-        `query_string` is a free text, non field-specific, query string.
+        `text` is a free text, non field-specific, query string.
         For more complex queries, use one of the factory methods.
 
         """
-        self.query_string = query_string
+        self.text = text
 
     def to_params(self):
-        return ['query_parse', self.query_string]
+        return ['freetext', self.text]
+
+class GeoDistanceQuery(QueryPart):
+    def __init__(self, centre=None):
+        """Create a query object, which weights documents by distance.
+
+        `centre` is a text representation of a latlong coordinate.
+
+        """
+        self.centre = centre
+
+    def to_params(self):
+        return ['geodistance', self.centre]
+
 
 # FIXME - implement more query types
