36ca417a85dd44651c005088f98ed29b4f247417
Matthew Harris <matt@themattharris.com>
2008-11-26 21:57:56 +0000

    Template tag now accepts arguments for configuring

--- zoo/trips/templatetags/trip_sightings.py
+++ zoo/trips/templatetags/trip_sightings.py
@@ -1,7 +1,10 @@
 from django import template
+from django.contrib.auth.models import User
 
 register = template.Library()
 
 @register.inclusion_tag('trips/_trip_sightings.html')
-def trip_sightings(trip):
-    return { 'trip': trip, }
+def trip_sightings(trip, htag, link_title):
+    return { 'trip': trip,
+             'htag': htag,
+             'link_title': link_title }
