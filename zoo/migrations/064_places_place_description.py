2c09ac9f08f7b2f2c083fa765644d107ecf336e2
Matthew Somerville <matthew@dracos.co.uk>
2008-11-26 11:47:32 +0000

    Typo

--- zoo/migrations/064_places_place_description.py
+++ zoo/migrations/064_places_place_description.py
@@ -5,6 +5,7 @@ elif settings.DATABASE_ENGINE == 'sqlite3':
     from dmigrations.sqlite3 import migrations as m
 import datetime
 
-migration = m.AddColumn('places', 'place', 'description', 'longtext NOT NULL default ""')
-migration = m.AddColumn('places', 'place', 'url', 'varchar(200) NOT NULL default ""')
-
+migration = m.Compound([
+    m.AddColumn('places', 'place', 'description', 'longtext NOT NULL default ""')
+    m.AddColumn('places', 'place', 'url', 'varchar(200) NOT NULL default ""')
+])
