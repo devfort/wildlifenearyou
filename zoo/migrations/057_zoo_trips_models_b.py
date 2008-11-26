5e178388450fb8a583c9260dc9345ad908271329
Matthew Somerville <matthew@dracos.co.uk>
2008-11-26 00:57:30 +0000

    Drop trips_trip.place_id

---
+++ zoo/migrations/057_zoo_trips_models_b.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+if settings.DATABASE_ENGINE == 'mysql':
+    from dmigrations.mysql import migrations as m
+elif settings.DATABASE_ENGINE == 'sqlite3':
+    from dmigrations.sqlite3 import migrations as m
+import datetime
+migration = m.Migration(
+    sql_up   = """ALTER TABLE `trips_trip` DROP COLUMN `place_id`""",
+    sql_down = """ALTER TABLE `trips_trip` ADD `place_id` integer NOT NULL REFERENCES `place` (`id`)"""
+])
