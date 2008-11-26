ac5a6b14eada34a59715441e76945326c1172168
Simon Willison <simon@simonwillison.net>
2008-11-26 20:54:45 +0000

    Lookup methods for getting details about a Xapian document by ID

--- zoo/search/searches.py
+++ zoo/search/searches.py
@@ -1,15 +1,26 @@
 from django.conf import settings
 from djape.client import Client, Query
 
-def search_species(q, num=0):
-    client = Client(settings.XAPIAN_BASE_URL, settings.XAPIAN_SPECIES_DB)
-    results = client.search(Query(q), end_rank=num)
-    for item in results['items']:
-        yield {
-            u'search_id': item['id'],
-            u'common_name': item['data']['common_name'][0],
-            u'scientific_name': item['data']['scientific_name'][0],
-        }
+class NotFound(Exception):
+    pass
+
+def make_lookup(dbname, latlon_fields=[]):
+    client = Client(settings.XAPIAN_BASE_URL, dbname)
+    def lookup(id):
+        result = client.get(id)
+        if len(result['items']) == 1:
+            item = result['items'][0]
+            doc = {u'search_id': item['id']}
+            for key, value in item['data'].items():
+                value = value[0]
+                if key in latlon_fields:
+                    value = map(float, value.split(' '))
+                doc[key] = value
+            return doc
+            # TODO: refactor common logic here and in make_searcher
+        else:
+            raise NotFound, id
+    return lookup
 
 def make_searcher(dbname, latlon_fields=[]):
     client = Client(settings.XAPIAN_BASE_URL, dbname)
@@ -28,3 +39,9 @@ def make_searcher(dbname, latlon_fields=[]):
 search_locations = make_searcher(
     settings.XAPIAN_LOCATION_DB, latlon_fields = ['latlon']
 )
+lookup_location = make_lookup(
+    settings.XAPIAN_LOCATION_DB, latlon_fields = ['latlon']
+)
+
+search_species = make_searcher(settings.XAPIAN_SPECIES_DB)
+lookup_species = make_lookup(settings.XAPIAN_SPECIES_DB)
