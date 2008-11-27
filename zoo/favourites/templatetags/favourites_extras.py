2f0ecc70e0214b064d365cdc6006966a84309deb
James Aylett <james@tartarus.org>
2008-11-27 22:24:28 +0000

    Tidy up profile page, as driven by Nat.

--- zoo/favourites/templatetags/favourites_extras.py
+++ zoo/favourites/templatetags/favourites_extras.py
@@ -3,7 +3,10 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('tags/favourite_toggler.html')
-def favourite_toggler(action, slug, next):
+def favourite_toggler(action, slug, next, klass='', add_label='Add to favourites', remove_label='Remove from favourites'):
     return {'action': action,
             'slug': slug,
-            'next': next}
+            'next': next,
+            'class': klass,
+            'add_label': add_label,
+            'remove_label': remove_label,}
