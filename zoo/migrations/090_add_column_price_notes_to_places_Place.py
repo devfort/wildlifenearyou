d3d65d99eb4fb80fd59d225aa0ba4ee3f107ddda
Matthew Harris <matt@themattharris.com>
2008-11-27 01:20:30 +0000

    Renumbered migration to 090, if you get errors mark_as_applied it just adds a column

--- zoo/migrations/090_add_column_price_notes_to_places_Place.py
+++ zoo/migrations/090_add_column_price_notes_to_places_Place.py
@@ -4,4 +4,4 @@ if settings.DATABASE_ENGINE == 'mysql':
 elif settings.DATABASE_ENGINE == 'sqlite3':
     from dmigrations.sqlite3 import migrations as m
 import datetime
-migration = m.AddColumn('places', 'Place', 'price_notes', 'longtext NULL')
+migration = m.AddColumn('places', 'place', 'price_notes', 'longtext NULL')
