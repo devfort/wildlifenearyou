403fe25abe0faa3a93911811414c53bea65b9cae
Simon Willison <simon@simonwillison.net>
2008-11-27 02:53:42 +0000

    Search, the UI appears but no results yet

--- zoo/search/views.py
+++ zoo/search/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from zoo.shortcuts import render
+#from zoo.search import search_places
+def search_places(q):
+    pass
+
+def search(request):
+    q = request.GET.get('q', '')
+    results = None
+    if q:
+        results = search_places(q)
+    
+    return render(request, 'search/search.html', {
+        'q': q,
+        'results': results,
+    })
