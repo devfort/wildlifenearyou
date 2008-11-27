6135bc1b351efb635d8afbac9c48145a6555c1d0
Nat Downe <natbat@gmail.com>
2008-11-27 03:55:22 +0000

    crazeee (adding css and js)

---
+++ zoo/static/js/global.js
@@ -0,0 +1,201 @@
+
+
+
+jQuery(function($) {
+	
+	
+	//	Automatically clear default text in input fields when they are focused
+	$("input[type='text']").focus(function() {
+		console.log('focus');
+		if ($(this).val() == this.defaultValue) {
+			$(this).val('');
+		}
+	});
+	//	Reinstate the default value if nothing has been entered.
+	$("input[type='text']").blur(function() {
+		if ($(this).val() == '') {
+			$(this).val(this.defaultValue);
+		}
+	});
+	
+	//	Make the targets of internal links toggleable.
+	$('a.toggler').each(function() {
+		//	Get the id of the target of each internal link.
+		var selector = $(this).attr('href');
+		if ($(selector)) {
+			$(selector).toggle();
+			//	When an internal link is clicked, toggle the display of the target.
+			$(this).click(function() {
+				var selector = $(this).attr('href');
+				$(selector).css({'overflow':'hidden'});
+				$(selector).slideToggle('fast');
+				$(this).toggleClass('displaying');
+				return false;
+			});
+		}
+	});
+	
+	// Toggleability of inputs (should work on radio and checkbuttons)
+	$('input.toggler').each(function() {
+		
+		//expecting sourcecode that looks like:
+		/*
+		<fieldset class="togglerContainer">
+			<div>
+				<label for="billing-yes">
+					<input name="billing-copy" value="yes" type="radio" id="billing-yes" checked="checked" /> 
+					Yes - copy over my delivery address
+				</label>
+			</div>
+			<div>
+				<label for="billing-no">
+					<input name="billing-copy" value="no" type="radio" class="toggler" id="billing-no"/> 
+					No - Ill add my billing address
+				</label>
+				<div class="dependent">
+					<label for="billing-street">Street and house</label>
+					<input type="text" class="text" name="billing-street" id="billing-street" />
+				</div>
+			</div>
+		</fieldset>
+		*/
+		// the input can be a sibling of the dependent too.
+		// this should also work with checkboxes
+		
+		// any radio or checkbox with a class of toggler 
+		// indicates its group has dependent elements embedded.
+		if(($(this).attr('type') == 'radio') || ($(this).attr('type') == 'checkbox')) {
+			
+			// hide all dependants
+			$($(this).parents('.togglerContainer')[0]).find('.dependent').hide();
+			
+			// get all the corresponding inputs
+			var inputgroup = $(this).attr('name');
+			
+			// check for default value and show dependents
+			$('[name="'+inputgroup+'"]').each(function(){
+				
+				if(this.checked) {
+					var dependentContainer; // jquery object
+					// show this dependent child
+					if($(this.parentNode).find('.dependent').length > 0) {
+						// our input has dependent code which is a sibling of our input
+						dependentContainer = $(this.parentNode);
+					} else if($(this.parentNode.parentNode).find('.dependent').length > 0) {
+						// our input might be inside another container like
+						// a label, go one up and look
+						dependentContainer = $(this.parentNode.parentNode);
+					}	
+				
+					if(dependentContainer) {
+						// show the one we want
+						dependentContainer.find('.dependent').show();
+					}
+				}
+			});
+			
+			// assign a click action to every element in that group
+			$('[name="'+inputgroup+'"]').click(function() {
+				// have clicked on an input in a toggle group.
+				
+				var togglerContainer = $(this).parents('.togglerContainer')[0];
+				var dependentContainer; // jquery object
+				
+				
+				// show this dependent child
+				if($(this.parentNode).find('.dependent').length > 0) {
+					// our input has dependent code which is a sibling of our input
+					dependentContainer = $(this.parentNode);
+				} else if($(this.parentNode.parentNode).find('.dependent').length > 0) {
+					// our input might be inside another container like
+					// a label, go one up and look
+					dependentContainer = $(this.parentNode.parentNode);
+				}
+				
+				if(dependentContainer) {
+					// if there are things we want to show
+					//console.log('there are things we want to show')
+					
+					// if the thing that we want to show is not already showing
+					if(!dependentContainer.find('.dependent:visible').length) {
+						// hide all other dependent childeren
+						$(togglerContainer).find('.dependent').slideUp('slow');
+						// show the one we want
+						dependentContainer.find('.dependent').slideDown('slow');
+					}
+				} else {	
+					// hide all other dependent childeren
+					$(togglerContainer).find('.dependent').slideUp('slow');
+				}
+			});
+		}
+
+	});
+
+	
+});
+
+// add JavaScript class for styling
+$('body').addClass('hasJS');
+
+
+
+// add classes to odd table rows
+$('table tbody tr:nth-child(odd)').addClass('odd');
+
+jQuery(window).load(function() {
+	// Crazyweird fix lets us style abbr using CSS in IE - do NOT run onDomReady, must be onload
+	document.createElement('abbr');
+});
+
+jQuery(function($){
+	
+	// addSizes was written by Natalie Downe 
+	// http://natbat.net/2008/Aug/27/addSizes/
+	
+	// Copyright (c) 2008, Natalie Downe under the BSD license
+	// http://www.opensource.org/licenses/bsd-license.php
+	
+	$('a[href$=".pdf"], a[href$=".doc"], a[href$=".mp3"], a[href$=".jpg"], a[href$=".png"], a[href$=".swf"], a[href$=".zip"], a[href$=".ogg"], a[href$=".m4u"]').each(function(){
+		// looking at the href of the link, if it contains pdf, doc, zip, mp3, ogg, m4u, jpg, png, swf
+		var link = $(this);
+		var bits = this.href.split('.');
+		var type = bits[bits.length -1];
+		
+		var url= "http://json-head.appspot.com/?url="+encodeURIComponent (this.href)+"&callback=?";
+	
+		// then call the json thing and insert the size back into the link text
+		 $.getJSON(url, function(json){
+			if(json.ok && json.headers['Content-Length']) {
+				var length = parseInt(json.headers['Content-Length'], 10);
+				
+				// divide the length into its largest unit
+				var units = [
+					[1024 * 1024 * 1024, 'GB'],
+					[1024 * 1024, 'MB'],
+					[1024, 'KB'],
+					[1, 'bytes']
+				];
+				
+				for(var i = 0; i < units.length; i++){
+					
+					var unitSize = units[i][0];
+					var unitText = units[i][1];
+					
+					if (length >= unitSize) {
+						length = length / unitSize;
+						// 1 decimal place
+						length = Math.ceil(length * 10) / 10;
+						var lengthUnits = unitText;
+						break;
+					}
+				}
+				
+				// insert the text directly after the link and add a class to the link
+				// note: if you want to insert the size into the link rather than after it change the following 'after' to 'append'
+				link.after(' (' + type + ' ' + length + ' ' + lengthUnits + ')');
+				link.addClass(type);
+			}
+		});
+	});
+});
---
+++ zoo/static/js/star-select-replace.js
@@ -0,0 +1,48 @@
+function selectReplacement(obj) {
+	obj.className += ' replaced';
+	var ul = document.createElement('ul');
+	ul.className = 'selectReplacement';
+	var opts = obj.options;
+	for (var i=0; i<opts.length; i++) {
+		var selectedOpt;
+		if (opts[i].selected) {
+			selectedOpt = i;
+			break;
+		} else {
+			selectedOpt = 0;
+		}
+	}
+	for (var i=0; i<opts.length; i++) {
+		var li = document.createElement('li');
+		var link = document.createElement('a');
+		li.appendChild(link);
+		li.className = opts[i].className;
+		link.selIndex = opts[i].index;
+		link.selectID = obj.id;
+		link.setAttribute('href','#');
+		link.onclick = function() {
+			selectMe(this);
+			return false;
+		}
+		if (i == selectedOpt) {
+			ul.className = 'selectReplacement '+opts[i].className;
+		}
+		ul.appendChild(li);
+	}
+	obj.parentNode.insertBefore(ul,obj);
+}
+function selectMe(obj) {
+	setVal(obj.selectID, obj.selIndex);
+	var list = obj.parentNode.parentNode;
+	list.className = 'selectReplacement '+obj.parentNode.className;
+}
+function setVal(objID, selIndex) {
+	var obj = document.getElementById(objID);
+	obj.selectedIndex = selIndex;
+}
+
+jQuery(document).ready(function() {
+	if (document.getElementById('review-rating')) {
+		selectReplacement(document.getElementById('review-rating'));
+	}
+});
