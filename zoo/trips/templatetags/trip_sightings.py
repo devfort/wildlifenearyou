9b46aa52edb68d75dd90c8e7a9be0f364f64f57a
Matthew Somerville <matthew@dracos.co.uk>
2008-11-27 19:53:53 +0000

    Sort out heading structure on trips

--- zoo/trips/templatetags/trip_sightings.py
+++ zoo/trips/templatetags/trip_sightings.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 register = template.Library()
 
 @register.inclusion_tag('trips/_trip_sightings.html')
-def trip_sightings(trip, htag, htag2, link_title):
+def trip_sightings(trip, htag, link_title):
     return { 'trip': trip,
              'htag': htag,
-             'htag2': htag2,
+             'htag2': htag+1,
+             'htag3': htag+2,
              'link_title': link_title }
