215e0c7767d2f916d48b279a259fa2443df533de
Matthew Somerville <matthew@dracos.co.uk>
2008-11-28 02:15:17 +0000

    Footer location AWESOMENESS

--- zoo/context_processors.py
+++ zoo/context_processors.py
@@ -1,10 +1,41 @@
 from django.core.cache import cache
+
 from zoo.animals.models import Species
+from zoo.utils import location_from_request
+from zoo.search import nearest_places_with_species
+
+from random import randint
 
 def standard(request):
     num = cache.get('total_num_of_species')
     if not num:
         num = Species.objects.all().count()
         cache.set('total_num_of_species', num, 60)
+
+    rand = randint(1, 2)
+    location, (lat, lon) = location_from_request(request)
+    animal = None
+    if location and rand==1:
+        animal = cache.get('footer_stat_nearest_%s' % location)
+        if not animal:
+            random_animal = Species.objects.order_by('?')[0]
+            nearest = nearest_places_with_species(random_animal.common_name, (lat, lon))
+            if nearest:
+                animal = {
+                    'name': random_animal.common_name,
+                    'url': random_animal.urls.absolute,
+                    'dist': int(nearest[0].distance_miles+0.5),
+                    'place_url': nearest[0].urls.absolute,
+                    'place_name': nearest[0],
+                }
+                cache.set('footer_stat_nearest_%s' % location, animal, 300)
+        else:
+            rand = 2
+    elif not location:
+        rand = 2
+
     return {'base': 'base.html',
-            'total_num_of_species': num}
+            'total_num_of_species': num,
+            'rand': rand,
+            'random_animal': animal
+    }
