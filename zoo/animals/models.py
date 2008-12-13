6f91ee1270f6845dc0bd3b35d354b04e624f20d4
Simon Willison <simon@simonwillison.net>
2008-12-13 18:52:45 +0000

    Fixed dumb bug - we were showing every photo from a trip on which a tiger was sighted, instead of every photo that was of a tiger

--- zoo/animals/models.py
+++ zoo/animals/models.py
@@ -62,7 +62,7 @@ class Species(AbstractSpecies):
     def visible_photos(self):
         from zoo.photos.models import Photo
         return Photo.objects.filter(
-            trip__sightings__species = self, is_visible = True,
+            sightings__species = self, is_visible = True,
         ).distinct()
 
     def random_photo(self):
