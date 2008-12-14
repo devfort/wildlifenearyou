3b96700cd9f32e57faf94e8805730e05b156c25a
Simon Willison <simon@simonwillison.net>
2008-12-14 22:45:56 +0000

    autocomplete is now a reserved username

--- zoo/configs/common_settings.py
+++ zoo/configs/common_settings.py
@@ -138,7 +138,7 @@ RESERVED_USERNAMES = set(
     group groups bulletin bulletins messages message newsfeed events 
     companies active rss img company books shop sales animals species 
     plants narwhals plant animal wildlife latin dictionary fish zoos zoo 
-    aquarium aquariums park parks safari arboretum arboretums
+    aquarium aquariums park parks safari arboretum arboretums autocomplete
     """.strip().split()
 )
 
