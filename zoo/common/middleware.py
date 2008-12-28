7ba681fddfd46a7cf0491348caa7010b641d7c58
James Aylett <james@tartarus.org>
2008-12-28 20:19:10 +0000

    Remove our explicit exception guard around our custom middleware, as our ext.django has this done centrally.

--- zoo/common/middleware.py
+++ zoo/common/middleware.py
@@ -6,68 +6,57 @@ since middleware is only executed by a first incoming web request and hence
 is not triggered for command line tools or the ./manage.py shell. It will have
 to do for the moment though.
 """
-# Wrap everything and catch exceptions, and die completely if they happen.
-try:
-    from django.http import HttpResponseRedirect
-    from django.conf import settings
-    from django.db.models.signals import pre_save, post_save
-    import datetime, threading
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.db.models.signals import pre_save, post_save
+import datetime, threading
 
-    from django.contrib.auth.models import User
-    from accounts.models import profilecalc_postsave
+from django.contrib.auth.models import User
+from accounts.models import profilecalc_postsave
 
-    ##### Only allow lowercase URLs on this site, redirect if contains uppercase
+##### Only allow lowercase URLs on this site, redirect if contains uppercase
 
-    class OnlyLowercaseUrls:
-        def process_request(self, request):
-            if request.path.lower() != request.path:
-                return HttpResponseRedirect(request.path.lower())
+class OnlyLowercaseUrls:
+    def process_request(self, request):
+        if request.path.lower() != request.path:
+            return HttpResponseRedirect(request.path.lower())
 
-    ##### Save current user in a thread local and auto-populate created_by etc
+##### Save current user in a thread local and auto-populate created_by etc
 
-    stash = threading.local()
-    def set_current_user(user):
-        stash.current_user = user
+stash = threading.local()
+def set_current_user(user):
+    stash.current_user = user
 
-    set_current_user(None)
+set_current_user(None)
 
-    def onanymodel_presave(sender, **kwargs):
-        current_user = stash.current_user
-        if current_user is None or not current_user.is_authenticated():
-            # this will throw an exception if there's no sedf user AND THIS IS A
-            # GOOD THING
-            current_user = User.objects.get(username='sedf')
+def onanymodel_presave(sender, **kwargs):
+    current_user = stash.current_user
+    if current_user is None or not current_user.is_authenticated():
+        # this will throw an exception if there's no sedf user AND THIS IS A
+        # GOOD THING
+        current_user = User.objects.get(username='sedf')
 
-        obj = kwargs['instance']
-        if hasattr(obj, 'modified_at'):
-            obj.modified_at = datetime.datetime.now()
-        if hasattr(obj, 'modified_by_id'):
-            obj.modified_by = current_user
-        if not obj.pk:
-            if hasattr(obj, 'created_at'):
-                obj.created_at = datetime.datetime.now()
-            if hasattr(obj, 'created_by_id'):
-                obj.created_by = current_user
+    obj = kwargs['instance']
+    if hasattr(obj, 'modified_at'):
+        obj.modified_at = datetime.datetime.now()
+    if hasattr(obj, 'modified_by_id'):
+        obj.modified_by = current_user
+    if not obj.pk:
+        if hasattr(obj, 'created_at'):
+            obj.created_at = datetime.datetime.now()
+        if hasattr(obj, 'created_by_id'):
+            obj.created_by = current_user
 
-    pre_save.connect(onanymodel_presave)
+pre_save.connect(onanymodel_presave)
 
-    class AutoCreatedAndModifiedFields:
-        def process_request(self, request):
-            set_current_user(request.user)
+class AutoCreatedAndModifiedFields:
+    def process_request(self, request):
+        set_current_user(request.user)
 
-    ##### Profiles have a percentage completion which needs recalculating when certain related models change
-    post_save.connect(profilecalc_postsave)
+##### Profiles have a percentage completion which needs recalculating when certain related models change
+post_save.connect(profilecalc_postsave)
 
-    ##### Hook up searchify/djape magic
-    if settings.SEARCH_ENABLED:
-        from searchify import initialise
-        initialise()
-
-except:
-    try:
-        import traceback
-        traceback.print_exc()
-        print "ERROR IN MIDDLEWARE INITIALISATION; DYING"
-    finally:
-        import os
-        os._exit(-1)
+##### Hook up searchify/djape magic
+if settings.SEARCH_ENABLED:
+    from searchify import initialise
+    initialise()
