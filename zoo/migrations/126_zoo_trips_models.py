9ea8639a0eb31393c130d8a62fd198c6d729f2e8
Simon Willison <simon@simonwillison.net>
2008-12-30 20:00:51 +0000

    Fix #188 - migration 126 now attempts to preserve data. Not fully tested yet though, so backup first!

--- zoo/migrations/126_zoo_trips_models.py
+++ zoo/migrations/126_zoo_trips_models.py
@@ -4,24 +4,59 @@ if settings.DATABASE_ENGINE == 'mysql':
 elif settings.DATABASE_ENGINE == 'sqlite3':
     from dmigrations.sqlite3 import migrations as m
 import datetime
-migration = m.Migration(sql_down=["""
-    CREATE TABLE `trips_inexactsighting` (
-        `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        `created_at` datetime NOT NULL,
-        `created_by_id` integer NOT NULL,
-        `modified_at` datetime NOT NULL,
-        `modified_by_id` integer NOT NULL,
-        `species` varchar(100) NOT NULL,
-        `place_id` integer NOT NULL,
-        `trip_id` integer NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-    ;
-""", """
-    ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT created_by_id_refs_id_18e810ff FOREIGN KEY (`created_by_id`) REFERENCES `auth_user` (`id`);
-""", """
-    ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT modified_by_id_refs_id_18e810ff FOREIGN KEY (`modified_by_id`) REFERENCES `auth_user` (`id`);
-""", """
-    ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT place_id_refs_id_5b85ed4 FOREIGN KEY (`place_id`) REFERENCES `places_place` (`id`);
-"""], sql_up=["""
-    DROP TABLE `trips_inexactsighting`;
-"""])
+
+class CustomMigration(m.Migration):
+    def __init__(self):
+        pass
+    
+    def up(self):
+        # First we convert existing inexact sightings in to regulars
+        existing = self.execute_sql(
+            """
+            SELECT 
+                created_at,
+                created_by_id,
+                species,
+                place_id,
+                trip_id,
+                modified_at,
+                modified_by_id
+            FROM trips_inexactsighting
+            """, return_rows=True
+        )
+        self.execute_sql(["""
+            DROP TABLE `trips_inexactsighting`;
+        """])
+        # Now insert them all again
+        for row in existing:
+            self.execute_sql(["""
+                INSERT INTO `trips_sighting` 
+                    (created_at, created_by_id, species_inexact, 
+                    place_id, trip_id, modified_at, modified_by_id)
+                VALUES
+                    ('%s', '%s', '%s', '%s', '%s', '%s', '%s')
+            """ % row])
+    
+    def down(self):
+        self.execute_sql([
+        """
+            CREATE TABLE `trips_inexactsighting` (
+                `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                `created_at` datetime NOT NULL,
+                `created_by_id` integer NOT NULL,
+                `modified_at` datetime NOT NULL,
+                `modified_by_id` integer NOT NULL,
+                `species` varchar(100) NOT NULL,
+                `place_id` integer NOT NULL,
+                `trip_id` integer NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+            ;
+        """, """
+            ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT created_by_id_refs_id_18e810ff FOREIGN KEY (`created_by_id`) REFERENCES `auth_user` (`id`);
+        """, """
+            ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT modified_by_id_refs_id_18e810ff FOREIGN KEY (`modified_by_id`) REFERENCES `auth_user` (`id`);
+        """, """
+            ALTER TABLE `trips_inexactsighting` ADD CONSTRAINT place_id_refs_id_5b85ed4 FOREIGN KEY (`place_id`) REFERENCES `places_place` (`id`);
+        """]
+        )
+migration = CustomMigration()
