d63aa89ca4e7954188239b0c803fee2a5d99e4c4
James Aylett <james@tartarus.org>
2009-07-12 11:45:50 +0000

    I assume this is a python 2.4-only problem, or would have been noticed by Simon: make sure everything we're concatenating in string context is a string when building the WSGI error log message.

--- zoo/common/wsgi_error_middleware.py
+++ zoo/common/wsgi_error_middleware.py
@@ -3,5 +3,5 @@ import traceback
 class WsgiLogErrors(object):
     def process_exception(self, request, exception):
         tb_text = traceback.format_exc()
-        url = request.build_absolute_uri(),
-        request.META['wsgi.errors'].write(url + '\n' + tb_text + '\n')
+        url = request.build_absolute_uri()
+        request.META['wsgi.errors'].write(url + '\n' + str(tb_text) + '\n')
