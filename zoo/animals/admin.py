12da43f8de1e14e6d99b65811f7f6acb7aa77360
Simon Willison <simon@simonwillison.net>
2010-01-02 14:33:26 +0000

    show freebase_id in species admin list

--- zoo/animals/admin.py
+++ zoo/animals/admin.py
@@ -3,7 +3,9 @@ from zoo.animals.models import Species, SpeciesGroup, SuperSpecies
 from zoo.common.models import exclude as excludes
 
 admin.site.register(Species,
-    list_display = ('common_name', 'species_group', 'latin_name', 'slug'),
+    list_display = (
+        'common_name', 'species_group', 'latin_name', 'slug', 'freebase_id'
+    ),
     list_filter = ['species_group', 'featured'],
     search_fields = ['common_name', 'latin_name'],
     prepopulated_fields = {'slug': ('common_name',)},
