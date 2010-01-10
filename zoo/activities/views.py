b40ef250a2abb69fce000943b8a9bc4bfbdbbd41
Simon Willison <simon@simonwillison.net>
2010-01-10 11:11:41 +0000

    Show 200 recent activities

--- zoo/activities/views.py
+++ zoo/activities/views.py
@@ -3,5 +3,5 @@ from models import Event
 
 def recent(request):
     return render('recent_activities.html', {
-        'events': Event.objects.order_by('-created')[:30],
+        'events': Event.objects.order_by('-created')[:200],
     })
