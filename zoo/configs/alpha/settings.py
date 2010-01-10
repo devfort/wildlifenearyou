53d3f73a8bed44f788d1016047c5f5194d3f73bf
Simon Willison <simon@simonwillison.net>
2010-01-10 16:23:01 +0000

    Moved Google Analytics code to the top, switched to using Asynchronous Tracking http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html

--- zoo/configs/alpha/settings.py
+++ zoo/configs/alpha/settings.py
@@ -45,14 +45,19 @@ DEV_STATUS_HTML = """
 
 GOOGLE_ANALYTICS_CODE = """
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-6697903-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-12396673-1']);
+_gaq.push(['_trackPageview']);
+_gaq.push(['t2._setAccount', 'UA-6697903-1']); // James
+_gaq.push(['t2._trackPageview']);
+(function() {
+var ga = document.createElement('script');
+ga.src = ('https:' == document.location.protocol ?
+    'https://ssl' : 'http://www') +
+    '.google-analytics.com/ga.js';
+ga.setAttribute('async', 'true');
+document.documentElement.firstChild.appendChild(ga);
+})();</script>
 """
 
 HTTP_PORT = 80
