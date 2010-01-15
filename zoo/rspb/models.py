960fe531658bba6a7cd22234a65d3511ddcee2a9
Simon Willison <simon@simonwillison.net>
2010-01-15 19:23:38 +0000

    Added RSPB snippet to species pages, where relevant

--- zoo/rspb/models.py
+++ zoo/rspb/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class RspbBirdPage(models.Model):
     name = models.CharField(max_length = 255)
     species = models.ManyToManyField(
-        'animals.Species', blank = True, null = True
+        'animals.Species', blank = True, null = True,
+        related_name = 'rspb_pages'
     )
     url = models.URLField(max_length = 255, verify_exists = False)
     image_url = models.CharField(max_length = 255, blank = True)
