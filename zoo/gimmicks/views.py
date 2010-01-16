2e30a6765afff1d4b166fbb9a5f9ba9b0642b457
Simon Willison <simon@simonwillison.net>
2010-01-16 17:23:47 +0000

    Gimmicks functionality is there - needs templating now

--- zoo/gimmicks/views.py
+++ zoo/gimmicks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render_to_response as render
 from models import Gimmick
 from places.models import Place
+from animals.models import Species
+
 from zoo.search.geocoders import google_geocode
 import geopy, geopy.distance
 
@@ -18,10 +20,11 @@ def gimmick(request, domain):
         name, (lat, lon) = google_geocode(q, country_code = country_code)
         return gimmick_results(request, gimmick, name, lat, lon)
     elif (lat and lon):
-        return gimmick_results(request, gimmick, None, lat, lon)
+        return gimmick_results(request, gimmick, '', lat, lon)
     else:
         return render('gimmicks/index.html', {
             'gimmick': gimmick,
+            'request_path': request.path,
         })
 
 def gimmick_results(request, gimmick, name, lat, lon):
@@ -46,5 +49,21 @@ def gimmick_results(request, gimmick, name, lat, lon):
     
     all_possible_places.sort(key = lambda p: p['distance'].km)
     
-    return all_possible_places
-
+    results = all_possible_places[:3]
+    
+    # Now load the actual ORM objects
+    place_pks = set([p['pk'] for p in results])
+    places = Place.objects.in_bulk(list(place_pks))
+    species_pks = set([p['animals_species_id'] for p in results])
+    species = Species.objects.in_bulk(list(species_pks))
+    
+    for place in results:
+        place['place'] = places[place['pk']]
+        place['species'] = species[place['animals_species_id']]
+    
+    return render('gimmicks/index.html', {
+        'gimmick': gimmick,
+        'results': results,
+        'name': name,
+        'request_path': request.path,
+    })
