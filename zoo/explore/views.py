36928b5c611042b07c2ba1d378cf8995e954a937
Simon Willison <simon@simonwillison.net>
2010-01-24 23:05:39 +0000

    /explore/ now shows recent photos of your favourite animals - this won't scale particularly well for users with lots of favourite animals, but it's OK for the moment

--- zoo/explore/views.py
+++ zoo/explore/views.py
@@ -2,8 +2,17 @@ from zoo.shortcuts import render
 from zoo.photos.models import Photo
 
 def index(request):
-    return render(request, 'explore/index.html', {
+    context = {
         'photos': Photo.objects.filter(
             is_visible = True
-        ).order_by('-created_at')[:32]
-    })
+        ).select_related('created_by').order_by('-created_at')[:16]
+    }
+    if not request.user.is_anonymous():
+        context['is_logged_in'] = True
+        context['photos_of_your_favourites'] = Photo.objects.filter(
+            is_visible = True
+        ).filter(
+            sightings__species__favourited__user = request.user
+        ).select_related('created_by').order_by('-created_at')[:16]
+    
+    return render(request, 'explore/index.html', context)
