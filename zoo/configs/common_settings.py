4a0b1baebd42e7d5d048e07c22468cccc8059254
Simon Willison <simon@simonwillison.net>
2010-03-07 15:26:35 +0000

    Reserve trip and trips as usernames

--- zoo/configs/common_settings.py
+++ zoo/configs/common_settings.py
@@ -197,7 +197,7 @@ RESERVED_USERNAMES = set(
     categories tag pick vote votes picks game games avatar avatars best
     explore better find pick choose chose winner winners loser losers lose win
     shorturl short shorturls user users metadata meta species-identifiers
-    bulk your-keys keys key
+    bulk your-keys keys key trip trips
     """.strip().split()
 )
 
